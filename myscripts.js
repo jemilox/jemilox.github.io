ece89ff07ad16d4a995177f487c192815dd3f647
jemilox <jemilox@gmail.com>
2016-06-17 22:01:44 -0500

    Create myscripts.js

---
+++ myscripts.js
@@ -0,0 +1,12 @@
+<HEAD> 
+<TITLE>Meow</TITLE> 
+
+<SCRIPT language="JavaScript"> 
+<!-- 
+alert('mew'); 
+alert('mew mew'); 
+alert('MEOW! MEOW! MEOW!'); 
+alert('RAWRRRRRRRR!!!!!!!!!!!!'); 
+//--> 
+</SCRIPT> 
+</HEAD>
