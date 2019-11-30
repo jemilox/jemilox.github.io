530780884552d2c7985a2ebffce10612e741087f
jemilox <jemilox@gmail.com>
2019-11-30 12:55:09 -0600

    serves up index.html

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+var http = require('http').Server(app);
+var path = require('path');
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/', function (req, res) {
+    console.log('base url hit');
+    res.sendFile(path.resolve('public/index.html'));
+  });
